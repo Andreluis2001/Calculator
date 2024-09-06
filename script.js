44746c5a76349caae602ee6993349cd8e31f9901
D3rK0nig45 <32471143+D3rK0nig45@users.noreply.github.com>
2024-09-06 08:38:39 -0300

    Final Project

--- script.js
+++ script.js
@@ -17,13 +17,13 @@ function divide(a, b){
 
 function operate(firstOperand, secondOperand, operation){
     switch(operation){
-        case 'add':
+        case '+':
             return add(firstOperand, secondOperand);
-        case 'subtract':
+        case '-':
             return subtract(firstOperand, secondOperand);
-        case 'multiply':
+        case '*':
             return multiply(firstOperand, secondOperand);
-        case 'divide':
+        case '/':
             return divide(firstOperand, secondOperand);
     }
 }
@@ -32,3 +32,43 @@ function operate(firstOperand, secondOperand, operation){
 let firstNumber = 0;
 let secondNumber = 0;
 let operator = "+";
+const display = document.querySelector(".display-text");
+const keys = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".op");
+for (let i = 0; i < keys.length; i++){
+        keys[i].addEventListener('click', function(){
+            if(display.textContent.length > 25){
+                return;
+            }
+            display.textContent += keys[i].textContent;
+        })
+}
+
+const clear_button = document.querySelector("#clear");
+clear_button.addEventListener("click", function(){
+    text_display = "";
+    display.textContent = text_display;
+    for (let j = 0; j < operators.length; j++){
+        operators[j].disabled = false;
+    }
+});
+
+for (let i = 0; i < operators.length; i++){
+    operators[i].addEventListener('click', function(e){
+        display.textContent += ' ' + e.target.textContent + ' ';
+        for(let j = 0; j < operators.length; j++){
+            operators[j].disabled = true;
+        }
+    })
+}
+
+const equals_button = document.querySelector("#equals");
+equals_button.addEventListener("click", function(e){
+    let operation = display.textContent.split(" ");
+    display.textContent = "";
+    display.textContent = operate(parseFloat(operation[0]), parseFloat(operation[2]), operation[1]);
+    for (let j = 0; j < operators.length; j++){
+        operators[j].disabled = false;
+    }
+})
+
